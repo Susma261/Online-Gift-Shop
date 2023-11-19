c6b67e59c6c68ef0f926996ae3de49983f4454b2
SUSMA R <69312974+Susma261@users.noreply.github.com>
2023-11-19 13:12:27 +0530

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.webp";
+import product2 from "./assets/products/2.webp";
+import product3 from "./assets/products/3.webp";
+import product4 from "./assets/products/4.webp";
+import product5 from "./assets/products/5.jpg";
+import product6 from "./assets/products/6.jpg";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "K1Portal Mug, Keychain Gift Set",
+    price: 199.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "OddClick Showpiece Gift Set",
+    price: 276.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Teddy Bear ",
+    price: 299.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Lvi Craft Soft Toy Gift Set",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Wallet Chain",
+    price: 399.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Metal Gold Plated Radha Krishna",
+    price: 699.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Golden Handcrafted Palm Buddha",
+    price: 729.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Home Decor Lucky Deer Family",
+    price: 899.0,
+    productImage: product8,
+  },
+];
